88b2b424fa5be97b2203e57a65cc9ede41d7f030
Mohamed Ait aadi <77589456+medfx69@users.noreply.github.com>
2023-03-11 10:10:05 +0000

    create some needed files

---
+++ http_TcpServer.cpp
@@ -0,0 +1,9 @@
+#include "http_TcpServer.hpp"
+
+http::TcpServer::TcpServer(/* args */)
+{
+}
+
+http::TcpServer::~TcpServer()
+{
+}
---
+++ http_TcpServer.hpp
@@ -0,0 +1,16 @@
+#ifndef HTTP_TCP_SERVER_HPP
+#define HTTP_TCP_SERVER_HPP
+namespace http
+{
+    class TcpServer
+    {
+        private:
+            /* data */
+        public:
+            TcpServer(/* args */);
+            ~TcpServer();
+    };
+} // namespace http
+
+
+#endif
