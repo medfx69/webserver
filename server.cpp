c77df6e27f6cd0df7ef894fa88ac39a16a5ddb84
medfx69 <mohamedox9080@gmail.com>
2023-03-26 14:38:06 +0000

    some of parcing handling

--- server.cpp
+++ server.cpp
@@ -6,11 +6,12 @@ int main(int ac, char **av){
     Parsed prq = pr;
 
 
-    std::cout << (*pr.getServer_data().block.begin()).block_name << std::endl;
-    (*pr.getServer_data().block.begin()).block_name = "hello";
-    std::cout <<">>> " << (*pr.getServer_data().block.begin()).block_name << std::endl;
-    std::cout << (*prq.getServer_data().block.begin()).block_name << std::endl;
-    std::cout << (*pr.getServer_data().block.begin()).block_name << std::endl;
+    std::cout << (*pr.getserver()->block.begin()).block_name << std::endl;
+    (*pr.getserver()->block.begin()).block_name = "hello";
+    std::cout <<">>> " << (*pr.getserver()->block.begin()).block_name << std::endl;
+    std::cout << (*prq.getserver()->block.begin()).block_name << std::endl;
+    std::cout << *(prq.getserver()->dir.end() - 1) << std::endl;
+    std::cout << (*pr.getserver()->block.begin()).block_name << std::endl;
     // http::TcpServer server = http::TcpServer("0.0.0.0", 8080);
     // server.startListen();
     return (0);
