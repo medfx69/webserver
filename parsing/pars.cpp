a08d49a206ffd37f9e214ebb511acf90f20881fd
Ali Bayar <abayar@e2r3p2.1337.ma>
2023-04-18 22:58:27 +0000

    parsing config 70%

--- parsing/pars.cpp
+++ parsing/pars.cpp
@@ -1,6 +1,6 @@
 #include "pars.hpp"
 
-server *Parsed::getDate() const
+std::vector<server> Parsed::getDate() const
 {
 	return handled_data;
 }
@@ -14,40 +14,39 @@ Parsed::Parsed(char *file)
 
 Parsed &Parsed::operator=(const Parsed &parsed)
 {
-	data_reader tmp;
-	std::vector<data_reader>::iterator it = parsed.getserver()->block.begin();
+	// data_reader tmp;
+	// // std::vector<data_reader>::iterator it = this->getserver()->block.begin();
 
-	delete s;
-	s = new data_reader();
-	while (it < parsed.getserver()->block.end())
-	{
-		s->block.push_back(*it);
-		it++;
-	}
-	s->block_name = parsed.getserver()->block_name;
-	std::vector<std::string>::iterator it2;
-	for (it2 = parsed.getserver()->dir.begin(); it2 < parsed.getserver()->dir.end(); it2++)
-	{
-		s->dir.push_back(*it2);
-	}
+	// delete s;
+	// s = new data_reader();
+	// while (it < parsed.getserver()->block.end())
+	// {
+	// 	s->block.push_back(*it);
+	// 	it++;
+	// }
+	// s->block_name = parsed.getserver()->block_name;
+	// std::vector<std::string>::iterator it2;
+	// for (it2 = parsed.getserver()->dir.begin(); it2 < parsed.getserver()->dir.end(); it2++)
+	// {
+	// 	s->dir.push_back(*it2);
+	// }
 	return *this;
 }
 Parsed::Parsed(const Parsed &s)
 {
-	this->s = new data_reader();
+	// this->s = new data_reader();
 	*this = s;
 }
 Parsed::Parsed()
 {
-
-	s = new data_reader();
+	// s = new data_reader();
 }
 
-data_reader *Parsed::getserver() const
+std::vector<data_reader> Parsed::getserver() const
 {
 	return s;
 }
-Parsed::~Parsed()
-{
-	delete s;
-}
+// Parsed::~Parsed()
+// {
+// 	// delete s;
+// }
