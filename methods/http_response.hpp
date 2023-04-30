de5cdfed88a1d2b43e665c25eeb9dad8d20dd636
floki61 <flo61ki@gmail.com>
2023-04-30 21:09:19 +0100

    some changes not finished

--- methods/http_response.hpp
+++ methods/http_response.hpp
@@ -1,13 +1,13 @@
 #ifndef HTTP_RESPONSE_HPP
 #define HTTP_RESPONSE_HPP
 
+#include <dirent.h>
 #include "../parsing/includes.hpp"
 #include "../http_TcpServer.hpp"
 
 class response
 {
     private:
-        // http::TcpServer *tcp;
     public:
         // response();
         std::string get_response(Parsed *data);
