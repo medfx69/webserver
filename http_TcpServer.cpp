6746a42296dd55425d83f9b8120f025f390f2448
medfx69 <mohamedox9080@gmail.com>
2023-05-04 16:20:42 +0100

    need some handling

--- http_TcpServer.cpp
+++ http_TcpServer.cpp
@@ -12,9 +12,7 @@ void exitWithError(const std::string &message)
 
 http::TcpServer::TcpServer(Parsed *data) : _data(data), m_socket(), m_new_socket()
 {
-    struct sockaddr_in                  m_socketAress;
-
-    for(size_t i = 0; i < _data->getDate().size(); i++){
+    for (size_t i = 0; i < _data->getDate().size(); i++){
         if ((_data->getDate()[i]).listen.first.find('.') != std::string::npos){
             m_ip_address.push_back((_data->getDate()[i]).listen.first);
             m_port.push_back(std::stoi((_data->getDate()[i]).listen.second));
@@ -24,18 +22,20 @@ http::TcpServer::TcpServer(Parsed *data) : _data(data), m_socket(), m_new_socket
             m_ip_address.push_back((_data->getDate()[i]).server_name);
         }
         m_socketAress.sin_family = AF_INET;
+        std::cout << "port :" << m_port[i] << std::endl;
         m_socketAress.sin_port = htons(m_port[i]);
+        std::cout << "addres :" << m_ip_address[i] << std::endl;
         m_socketAress.sin_addr.s_addr = inet_addr(m_ip_address[i].c_str());
-        // m_socketAress.sin_addr.s_addr = INADDR_ANY;
-        m_socketAddress_len.push_back(sizeof(m_socketAress));
+        m_socketAddress_len = sizeof(m_socketAress);
+        class_m_socketAddress_len.push_back(m_socketAddress_len);
         class_m_socketAress.push_back(m_socketAress);
-    }
         if (startServer() != 0)
         {
             std::ostringstream ss;
             ss << "Failed to start with PORT:" << ntohs(m_socketAress.sin_port);
             log(ss.str());
         }
+    }
 }
 
 http::TcpServer::~TcpServer()
@@ -45,22 +45,17 @@ http::TcpServer::~TcpServer()
 
 int http::TcpServer::startServer()
 {
+    int fd;
     int i = 1;
-    int option = 1;
-    for (size_t j = 0; j < class_m_socketAress.size(); j++){
-        i = socket(AF_INET, SOCK_STREAM, 0);
-        std::cout << "fd1 = " << i << std::endl;
-        if (i < 0)
-            exitWithError("Cannot create socket");
-        m_socket.push_back(i);
-        if (setsockopt(i, SOL_SOCKET, SO_REUSEADDR , &option, sizeof(option)) < 0)
-            exitWithError("----------setsockopt-----------");
-        // if (setsockopt(i, SOL_SOCKET, SO_NOSIGPIPE , &option, sizeof(option)) < 0)
-        //     exitWithError("----------setsockopt-----------");
-        if (bind(i, (struct sockaddr*)&class_m_socketAress[j], m_socketAddress_len[j]) < 0){
-            exitWithError("Cannot connect socket to address");
-        }
-    }
+
+    fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd < 0)
+        exitWithError("Cannot create socket");
+    if(setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, (char *)&i, sizeof(i)) < 0 )
+        exitWithError("----------setsockopt-----------");
+    if (bind(fd, (sockaddr *) &m_socketAress, m_socketAddress_len) < 0)
+        exitWithError("Cannot connect socket to address");
+    m_socket.push_back(fd);
     return 0;
 }
 
@@ -72,17 +67,25 @@ int setNonblocking(int fd)
 int http::TcpServer::acceptConnection(int fd, int c)
 {
     int new_socket;
-    new_socket = accept(fd, (sockaddr *)&class_m_socketAress[c], &m_socketAddress_len[c]);
+
+    m_socketAress = class_m_socketAress[c];
+    m_socketAddress_len = class_m_socketAddress_len[c];
+    std::cout << inet_ntoa(m_socketAress.sin_addr) << std::endl;
+    std::cout << ntohs(m_socketAress.sin_port) << std::endl;
+    new_socket = accept(fd, (sockaddr *)&m_socketAress, (socklen_t *) &m_socketAddress_len);
     if (new_socket < 0)
     {
+        std::cout << errno << std::endl;
         std::ostringstream ss;
         ss << "Server failed to accept incoming connection from ADDRESS: "
            << inet_ntoa(class_m_socketAress[c].sin_addr) << "; PORT: "
            << ntohs(class_m_socketAress[c].sin_port);
         exitWithError(ss.str());
     }
-    setNonblocking(new_socket);
-    m_new_socket.push_back(new_socket);
+    else{
+        setNonblocking(new_socket);
+        m_new_socket.push_back(new_socket);
+    }
     return new_socket;
 }
 
@@ -90,20 +93,18 @@ int http::TcpServer::acceptConnection(int fd, int c)
 int http::TcpServer::listening(){
     int max_fd;
 
-    for (std::vector<int>::iterator it = m_socket.begin(); it < m_socket.end(); it++)
-        if (listen(*it, SOMAXCONN) < 0)
+    max_fd = 0;
+    FD_ZERO(&readst);
+    for (size_t i = 0; i < m_socket.size(); i++){
+        std::cout << m_socket[i] << std::endl;
+        if (listen(m_socket[i], SOMAXCONN) < 0)
             exitWithError("Socket listen failed");
+        FD_SET(m_socket[i], &readst);
+        max_fd = m_socket[i];
+    }
+    FD_ZERO(&writest);
     timer.tv_sec = 4;
     timer.tv_usec = 0;
-    FD_ZERO(&readst);
-    FD_ZERO(&writest);
-    max_fd = 0;
-    for (std::vector<int>::iterator ite = m_socket.begin(); ite < m_socket.end(); ite++)
-    {
-        FD_SET(*ite, &readst);
-        max_fd = *ite;
-        std::cout << max_fd << std::endl;
-    }
     return max_fd;
 }
 
@@ -116,58 +117,61 @@ void http::TcpServer::save(int fd){
     reFile.close();
 }
 
+bool http::TcpServer::isMaster(int fd){
+    for (size_t i = 0; i < m_socket.size(); i++)
+        if(fd == m_socket[i])
+            return true;
+    return (false);
+}
+
 void http::TcpServer::startListen(Parsed *data){
     int                 bytesReceived, max_fd, max_fd_tmp, act, max_fd_check;
     fd_set              read_tmp, write_tmp;
     std::ostringstream  ss;
 
-    max_fd = listening();
     FD_ZERO(&read_tmp);
     FD_ZERO(&write_tmp);
+    max_fd = listening();
     max_fd_tmp = max_fd;
     while (true)
     {
-        int c = 0;
-        // for (std::vector<int>::iterator it = m_socket.begin(); it < m_socket.end(); it++){
-            // std::cout << ">>>>" << *it << std::endl;
+        for (size_t c = 0; c < m_socket.size(); c++){
             read_tmp = readst;
             write_tmp = writest;
-            log("====== Waiting for a new connection ======\n\n\n");
+            log("====== Waiting for a new connection ======\n");
             act = select(max_fd +1, &readst, &writest, NULL, NULL);
             if (act < 0)
                 exitWithError("--------select error-------");
             std::vector<std::pair<int, bool> > fds;
             for (int i = 0; i < max_fd + 1 ; i++){
                 if (FD_ISSET(i, &read_tmp) || FD_ISSET(i, &write_tmp)){
-                    if (i == m_socket[0]){
+                    if (isMaster(i)){
                         fds.push_back(std::make_pair(i, true));
-                        max_fd_check = acceptConnection(m_socket[0], c);
+                        max_fd_check = acceptConnection(i, c);
                         FD_SET(max_fd_check, &read_tmp);
                         if (max_fd_check > max_fd_tmp)
                             max_fd_tmp = max_fd_check;
                     }
                     else{
                         fds.push_back(std::make_pair(i, false));
-                        bytesReceived = recv(i, buffer, BUFFER_SIZE, 0);
+                        bytesReceived = read(i, buffer, BUFFER_SIZE);
                         save(i);
                         FD_SET(i, &write_tmp);
                         FD_CLR(i, &read_tmp);
                         (void)data;
                         buildResponse(data);
-                        std::ostringstream ss;
-                        log(ss.str());
-                        sendResponse(i);
+                        if (FD_ISSET(i, &write_tmp))
+                            sendResponse(i);
                         FD_CLR(i, &write_tmp);
                         // close(i);
 
                     }                  
                 }
-            // }
+            }
             for (size_t x = 0; x < fds.size(); x++){
-                std::cout << fds[x].first << ":" << fds[x].second << ";";
+                std::cout << fds[x].first << ":" << fds[x].second << ";  ";
             }
             std::cout << std::endl;
-            c++;
             max_fd = max_fd_tmp;
             readst = read_tmp;
             writest = write_tmp;
