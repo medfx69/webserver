142c9736332182b94571c2fd94c4356d1afcffcf
floki61 <flo61ki@gmail.com>
2023-05-04 21:45:26 +0100

    autoindex

--- methods/http_response.cpp
+++ methods/http_response.cpp
@@ -1,10 +1,9 @@
 #include "http_response.hpp"
-#include <sys/stat.h>
 
-std::string    CheckPathType(Parsed& data)
+std::string    response::CheckPathType(Parsed* data)
 {
     struct stat st;
-    if(stat(data.req->absoluteURI.c_str(), &st) != 0)
+    if(stat(data->req->absoluteURI.c_str(), &st) != 0)
         return "NOT FOUND";
     else if(S_ISDIR(st.st_mode))
         return "FOLDER";
@@ -12,7 +11,30 @@ std::string    CheckPathType(Parsed& data)
         return "FILE";
 }
 
-std::string getfile(std::string& pathfile)
+std::string response::createindexhtml(std::string pathdir)
+{
+    DIR* dir = opendir(pathdir.c_str());
+    if(dir != NULL)
+    {
+        std::ostringstream htmlfile;
+        htmlfile << "<!DOCTYPE html><html lang=\"en\"><body>";
+        struct dirent* entry;
+        while((entry = readdir(dir)) != NULL)
+        {
+            std::string fd = entry->d_name;
+            if(fd == "." || fd == "..")
+                ;
+            else
+                htmlfile << "<a href='" << "./" << fd << "'>" << fd << "</a><br /><br />" << std::endl;
+        }
+        closedir(dir);
+        htmlfile << "</body></html>";
+        return htmlfile.str();
+    }
+    return "HTTP/1.1 404 Not Found\r\n\r\n";
+}
+
+std::string response::getfile(std::string pathfile)
 {
     std::ifstream file(pathfile);
     if (!file.is_open()) {
@@ -25,45 +47,26 @@ std::string getfile(std::string& pathfile)
     return "HTTP/1.1 200 OK\r\n\r\n" + file_content.str();
 }
 
-std::string getfolder(Parsed& data, server& config)
+std::string response::getfolder(Parsed* data, server config)
 {
-    if(data.req->absoluteURI.back() != '/')
+    if(data->req->absoluteURI.back() != '/')
         return "HTTP/1.1 301 Moved Permanently\r\n\r\n";
     if (!config.index.empty())
-        return getfile(data.req->absoluteURI + "/" + config.index[0]);
+        return getfile(data->req->absoluteURI + "/" + config.index[0]);
     else if(config.autoindex == "OFF")
         return "HTTP/1.1 301 Moved Permanently\r\n\r\n";
     else if(config.autoindex == "ON")
-    {
-        DIR* dir = opendir(data.req->absoluteURI.c_str());
-        if(dir != NULL)
-        {
-            struct dirent* entry;
-            while((entry = readdir(dir)) != NULL)
-            {
-                if (entry->d_type == DT_REG)
-                {
-                    std::string file = entry->d_name;
-                    if(file == config.index[0])
-                        return getfile(file);
-                }
-            }
-            closedir(dir);
-        }
-    }
+        return createindexhtml(data->req->absoluteURI);
+    return NULL;
 }
 
-std::string   response::get_response(Parsed& data, server& config)
+std::string   response::get_response(Parsed* data, server config)
 {
-    std::string     m_serverMessage;
     std::string pathtype = CheckPathType(data);
     if(pathtype == "FILE")
-        return getfile(data.req->absoluteURI);
+        return getfile(data->req->absoluteURI);
     else if(pathtype == "FOLDER")
         return getfolder(data, config);
-    else {
-		m_serverMessage = "HTTP/1.1 404 Not Found\r\n\r\n";
-    }
-    return m_serverMessage;
+	return "HTTP/1.1 404 Not Found\r\n\r\n";
 }
 
--- methods/http_response.hpp
+++ methods/http_response.hpp
@@ -2,16 +2,19 @@
 #define HTTP_RESPONSE_HPP
 
 #include <dirent.h>
+#include <sys/stat.h>
 #include "../parsing/includes.hpp"
 #include "../http_TcpServer.hpp"
 
 class response
 {
-    private:
-    public:
-        std::string	get_response(Parsed& data, server& config);
-		std::string    CheckPathType(Parsed& data);
-
+	private:
+	public:
+		std::string	get_response(Parsed* data, server config);
+		std::string	getfolder(Parsed* data, server config);
+		std::string	getfile(std::string pathfile);
+		std::string	CheckPathType(Parsed* data);
+		std::string createindexhtml(std::string pathdir);
 };
 
 #endif
