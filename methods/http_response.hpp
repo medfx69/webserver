125f9e8a2c9bdcde727936fe8aea0e359dbfe38d
floki61 <flo61ki@gmail.com>
2023-05-06 23:03:01 +0100

    get file extension

--- methods/http_response.hpp
+++ methods/http_response.hpp
@@ -9,12 +9,29 @@
 class response
 {
 	private:
+		static const std::map<std::string, std::string> mimeTypeMap;
 	public:
+		response ();
 		std::string	get_response(Parsed* data, server config);
 		std::string	getfolder(Parsed* data, server config);
 		std::string	getfile(std::string pathfile);
-		std::string	CheckPathType(Parsed* data);
-		std::string createindexhtml(std::string pathdir);
+		std::string	checkPathType(Parsed* data);
+		std::string createIndexHtml(std::string pathdir);
+		std::string getFileExtension(const std::string& filePath);
+		std::string generateResponseHeader(const std::string& filePath);
+		std::string contentType(const std::string& filePath);
+};
+
+const std::map<std::string, std::string> response::mimeTypeMap = {
+    {"html", "text/html"},
+    {"htm", "text/html"},
+    {"css", "text/css"},
+    {"js", "application/javascript"},
+    {"jpg", "image/jpeg"},
+    {"jpeg", "image/jpeg"},
+    {"png", "image/png"},
+    {"gif", "image/gif"},
+    {"pdf", "application/pdf"}
 };
 
 #endif
