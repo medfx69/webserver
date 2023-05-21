b758a6214bdacaa3211bf92f1eaa3dd174e06646
floki61 <flo61ki@gmail.com>
2023-05-21 13:51:09 +0100

    push

--- methods/http_response.cpp
+++ methods/http_response.cpp
@@ -318,10 +318,10 @@ std::string   response::get_response()
     	// }
 
 	}
-	else if(req->method == "DELETE")
-	{
-		DELETE(req->absoluteURI);
-	}
+	// else if(req->method == "DELETE")
+	// {
+	// 	DELETE(req->absoluteURI);
+	// }
 	return errorPage(404);
 }
 
