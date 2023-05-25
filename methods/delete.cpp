82283b7c36628cc65af5eb484e32be038d2d8f63
floki61 <flo61ki@gmail.com>
2023-05-25 16:46:20 +0100

    delete pirm

--- methods/delete.cpp
+++ methods/delete.cpp
@@ -3,9 +3,9 @@
 void  response::f_remove(std::string path)
 {
   if(remove(path.c_str()))
-    std::cout << "204 No Content status code. " << path << std::endl;
+	std::cout << "204 No Content status code. " << path << std::endl;
   else
-    std::cout << path << " deleted" << std::endl;
+	std::cout << path << " deleted" << std::endl;
 }
 
 std::string response::path_creat(std::string path, std::string join_path)
@@ -13,47 +13,108 @@ std::string response::path_creat(std::string path, std::string join_path)
   return path += "/" + join_path;
 }
 
+bool hasWritePermission(const std::string& path) {
+    struct stat fileInfo;
+    if (stat(path.c_str(), &fileInfo) == 0) {
+        mode_t mode = fileInfo.st_mode;
+        // Check if the user has write permission
+        return (mode & S_IWUSR) != 0;
+    }
+    return false;
+}
+
+bool response::checkPermission(std::string path, std::string i)
+{
+	// struct stat st;
+	if(i == "FOLDER")
+	{
+		del jj;
+		DIR* dir = opendir(path.c_str());
+		if (dir != NULL)
+		{
+			struct dirent* entry;
+			while ((entry = readdir(dir)) != NULL)
+		  	{
+				if (entry->d_type == DT_REG)
+				{
+					jj.filename = path_creat(path, entry->d_name);
+					std::cout << "File found: " << jj.filename << std::endl;
+					if(!hasWritePermission(jj.filename))
+						return 0;
+				}
+				else if (entry->d_type == DT_DIR)
+				{
+					std::string fd = entry->d_name;
+					if(fd == "." || fd == "..")
+					  std::cout << entry->d_name << std::endl;
+					else
+					{
+						jj.foldername = path_creat(path, entry->d_name);
+						std::cout << "Subdirectory found: " << jj.foldername << std::endl;
+						if(!hasWritePermission(jj.foldername))
+							return 0;
+						if(!checkPermission(jj.foldername, "FOLDER"))
+							return 0;
+					} 
+				}
+		  	}
+			closedir(dir);
+			return 1;
+		}
+		else
+			return 0;
+	}
+	else if(i == "FILE")
+	{
+		if(!hasWritePermission(path.c_str()))
+			return 0;
+	}
+	else
+		return 0;
+	return 1;
+}
+
 bool response::DELETE(std::string path)
 {
-  std::string pathtype = checkPathType();
+	std::string pathtype = checkPathType();
 	if(pathtype == "FOLDER")
-  {
-    del jj;
-    DIR* dir = opendir(path.c_str());
-    if (dir != NULL)
-    {
-      struct dirent* entry;
-      while ((entry = readdir(dir)) != NULL)
-      {
-        if (entry->d_type == DT_REG)
-        {
-          jj.filename = path_creat(path, entry->d_name);
-          std::cout << "File found: " << jj.filename << std::endl;
-        }
-        else if (entry->d_type == DT_DIR)
-        {
-          std::string fd = entry->d_name;
-          f_remove(jj.filename);
-          if(fd == "." || fd == "..")
-            std::cout << entry->d_name << std::endl;
-          else
-          {
-            jj.foldername = path_creat(path, entry->d_name);
-            std::cout << "Subdirectory found: " << jj.foldername << std::endl;
-            DELETE(jj.foldername);
-            f_remove(jj.foldername);
-          } 
-        }
-      }
-      closedir(dir);
-      // return 1;
-    }
-    else
-      std::cout << "Folder not found or could not be opened" << std::endl;
-  }
+	{
+		del jj;
+		DIR* dir = opendir(path.c_str());
+		if (dir != NULL)
+		{
+			struct dirent* entry;
+			while ((entry = readdir(dir)) != NULL)
+		  	{
+				if (entry->d_type == DT_REG)
+				{
+					jj.filename = path_creat(path, entry->d_name);
+					std::cout << "File found: " << jj.filename << std::endl;
+					f_remove(jj.filename);
+				}
+				else if (entry->d_type == DT_DIR)
+				{
+					std::string fd = entry->d_name;
+					if(fd == "." || fd == "..")
+					  std::cout << entry->d_name << std::endl;
+					else
+					{
+					  jj.foldername = path_creat(path, entry->d_name);
+					  std::cout << "Subdirectory found: " << jj.foldername << std::endl;
+					  DELETE(jj.foldername);
+						f_remove(jj.foldername);
+					} 
+				}
+		  	}
+			closedir(dir);
+			return 1;
+		}
+		else
+			std::cout << "Folder not found or could not be opened" << std::endl;
+	}
 	else if(pathtype == "FILE")
-    f_remove(path);
-  return 0;
+		f_remove(path);
+	return 0;
 }
 // int main()
 // {
@@ -61,6 +122,3 @@ bool response::DELETE(std::string path)
 //   if(DELETE(fd))
 //     f_remove(fd);
 // }
-
-
-
